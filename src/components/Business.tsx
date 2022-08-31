3fd2dca27d0d221033dd5914eadeb9bc757135f4
SimonC989 <simonchan989@gmail.com>
2022-08-31 01:06:33 -0700

    Added bare components. Finished Navbar and Hero component.

---
+++ src/components/Business.tsx
@@ -0,0 +1,12 @@
+import { features } from '../constants'
+import styles, { layout } from '../styles'
+
+const Business = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Business
---
+++ src/components/Stats.tsx
@@ -0,0 +1,21 @@
+import { stats } from '../constants'
+import styles from '../styles'
+
+const Stats = () => {
+  return (
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+      {stats.map((stat) => 
+        <div key={stat.id} className={`flex-1 flex justift-start items-center flex-row m-3`}>
+          <h4 className='text-white font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px]'>
+            {stat.value}
+          </h4>
+          <p className='text-gradient font-poppins font-semibold xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] ml-3 uppercase'>
+            {stat.title}
+          </p>
+        </div>
+      )}
+    </section>
+  )
+}
+
+export default Stats
