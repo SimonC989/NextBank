4e0939a407b9dd8785e8c1f82399bd708012244c
SimonC989 <simonchan989@gmail.com>
2022-08-31 01:30:05 -0700

    Implemented Business component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Button = () => {
+export interface IButtonProps {
+  styles: string,
+}
+
+const Button = ({ styles }: IButtonProps) => {
   return (
-    <div>Button</div>
+    <button type='button' className={`${styles} py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-lg`}>
+      Get Started
+    </button>
   )
 }
 
